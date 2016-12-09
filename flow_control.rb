1e35389e5ce595a5dd58a68b817a70f5ca34fad9
Andrew James <ajames20@gmail.com>
2016-12-09 18:01:41 +0200

    flow control exercises

---
+++ flow_control.rb
@@ -0,0 +1,52 @@
+=begin
+exercise 1:
+Write down whether the following expressions return true or false. Then type the expressions into irb to see the results.
+
+1. (32 * 4) >= 129
+2. false != !true
+3. true == 4
+4. false == (847 == '874')
+5. (!true || (!(100 / 5) == 20) || ((328 / 4) == 82)) || false
+=end
+
+puts "1. False"
+puts "2. False"
+puts "3."
+puts "\n"
+
+=begin
+exercise 2:
+
+=end
+
+puts "\n"
+
+=begin
+exercise 3:
+
+=end
+
+puts "\n"
+
+=begin
+exercise 4:
+
+=end
+
+puts "\n"
+
+
+=begin
+exercise 5:
+
+=end
+
+puts "\n"
+
+
+=begin
+exercise 6:
+
+=end
+
+puts "\n"
